f0fe153b7e4662364a55f36142ebaf7eb73c36e8
Jitesh <jchavan010@gmail.com>
2025-06-19 16:55:30 -0400

    conditional path, alpha_t, beta_t construction. Conversion of vector_field models to denoising models, to score models, and conversion of score models to vector_field models

--- fmamba/flowMatching/path.py
+++ fmamba/flowMatching/path.py
@@ -48,7 +48,7 @@ class ICPlan:
         return 1 / t
 
     def compute_drift(self, x, t):
-        # TODO: I dont like this implementation, it uses wrong terminologies 
+        # TODO: I dont like this implementation, it uses wrong terminologies and conventions 
         # further negates the coefficient of x in the score parametrization of conditional vectorfield while returning the two coefficients
         """
         We always output VectorField ut_theta(x|z) according to score parametrization; (not to mention minimizer theta for this conditional object also minimizes the corresponding marginal object (refer lecture notes))
@@ -62,13 +62,17 @@ class ICPlan:
         # beta_t = 1 - t, beta_t_dot = -1
         beta_t, beta_t_dot = self.compute_beta_t(t)
         
-        # idk why we are pretending that coeff of x in the score parametrization is called drift
+        # idk why we are pretending that term of x in the score parametrization is called drift
         # idk why we are pretending that coeff of conditional score is called diffusion
         # idk why the heck we are returning negative of coefficient of x in the score parametrization, in formula its clearly addition
         drift = alpha_ratio * x
         diffusion = alpha_ratio * (beta_t**2) - beta_t * beta_t_dot
+        # why stray away from mathematical convention and negate for reason
         return -drift, diffusion
     
+    # not to be confused with what they call diffusion above lol
+    # looks like diffusion coefficient sigma_t
+    # twisted repository
     def compute_diffusion(self, x, t, form="constant", norm=1.0):
         """Compute the diffusion term of the SDE
         Args:
@@ -96,4 +100,79 @@ class ICPlan:
             raise NotImplementedError(f"Diffusion form {form}, rather the beta_t schedule, is not implemented")
 
         return diffusion
+    
+    def compute_d_diffusion(self, x, t, form="constant", norm=1.0):
+        """Compute the derivative of diffusion term of the SDE
+        Args:
+            x: (B, ...), data point x_t on the trajectory
+            t: (B), time
+            form: str, form of the diffusion term
+            norm: float, norm of the diffusion term
+        """
+        t = expand_t_like_x(t, x)
+        choices = {
+            "none": torch.zeros((1,), device=t.device),
+            "constant": torch.zeros((1,), device=t.device),
+            #"SBDM": norm * 2.0 * self.compute_drift(x, t)[1],
+            #"sigma": norm * self.compute_beta_t(t)[0],
+            "linear": torch.full((1,), -norm), # negative slope implies noise injection tends to zero towards t=1
+            "decreasing": -0.5
+            * np.pi
+            * norm
+            * torch.sin(np.pi * t)
+            * (norm * torch.cos(np.pi * t) + 1), # derivative of 0.25 * (norm * cos(pi * t) + 1) ** 2
+            "increasing-decreasing": 2
+            * norm
+            * np.pi
+            * torch.sin(np.pi * t)
+            * torch. cos(np.pi * t), # derivative of norm * sin (pi * t)^ 2
+            "log" : norm * (1 - 2 * t) / (t - t**2 + 1)
+        }
+
+        try:
+            diffusion = choices[form]
+        except KeyError:
+            raise NotImplementedError(f"Diffusion form {form} is not implemented")
+        return diffusion
+    
+    
+    def get_score_from_vector_field (self, vector_field, x, t):
+        """Transform vector field to score function
+        Args:
+            vector_field : (B, ...) shaped tensor
+            x: (B, ...); x_t data point on the trajectory at time t
+            t: (B) time 
+        """
+
+        t = expand_t_like_x(t, x) # (B, ...) matches shape of x_t
+        alpha_t, alpha_t_dot = self.compute_alpha_t(t)
+        beta_t, beta_t_dot = self.compute_beta_t(t)
+        mean = x # (B, ...)
+        reverse_alpha_ratio = alpha_t / alpha_t_dot
+        var = beta_t**2 - reverse_alpha_ratio * beta_t_dot * beta_t
+        score = (reverse_alpha_ratio * vector_field - mean) / var
+        return score
+    
+    # Just substitute Score Function with -eps/beta_t as derived in denoising score matching objective (conditional score matching objective)
+    def get_noise_from_vector_field(self, vector_field, x, t):
+        """Transform vector_field u_t_theta(x_t) model to denoiser
+        Args:
+            vector_field: (B, ...), vectorfield_model
+            x: (B, ...), x_t data point on the trajectory at time t
+            t: (B) time
+        """
+
+        t = expand_t_like_x(t, x) # (B, 1, 1, 1) in case x is (B, C, H, W)
+        alpha_t, alpha_t_dot = self.compute_alpha_t(t)
+        beta_t, beta_t_dot = self.compute_beta_t(t)
+
+        mean = x
+        reverse_alpha_ratio = alpha_t / alpha_t_dot
+        var = reverse_alpha_ratio * beta_t_dot - beta_t
+        noise = (reverse_alpha_ratio* vector_field - mean) / var
+        return noise
+    
+    
+
+    
     
